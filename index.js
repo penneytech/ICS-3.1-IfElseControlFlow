018fc0147c4c9263127ded150747e7f65319f806
penneytech <99091373+penneytech@users.noreply.github.com>
2024-09-09 09:21:09 -0400

    First commit.

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+You are making an app that will tell someone if the package they're mailing is too heavy or not.
+
+1) Declare a variable for the package weight. 
+2) Create an if / else statement that will log:
+    a) "The package is within weight limits!" if the package is under 15 lbs.
+    b) "The package is overweight" in cases where it is not under 15 lbs. 
+3) Set the initial value of the package weight to trigger the message in step 2. 
+*/
