1e19cbde199e309c93292047610a59e746355a29
penneytech <penneyp@hdsb.ca>
2025-02-06 10:55:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 /*
+
 You are making an app that will tell someone if the package they're mailing is too heavy or not.
 
-1) Declare a variable for the package weight. 
+1) Declare a variable for the package weight and set it to have an initial value of 0.
+
 2) Create an if / else statement that will log:
     a) "The package is within weight limits!" if the package is under 15 lbs.
     b) "The package is overweight" in cases where it is not under 15 lbs. 
-3) Set the initial value of the package weight to trigger the message in step 2. 
+
+3) Test your code by changing the value of the package weight variable and running the program.
+
+4) What happens if you change your package weight to 15? Does it work like it should? If not, attempt to fix it?
+
 */
